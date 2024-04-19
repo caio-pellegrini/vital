f7a4ecacb1be360762f2526e89eb5859e22359d9
Caio Pellegrini <caiopellegrini97@gmail.com>
2024-04-19 09:31:12 -0300

    Update translation for "Resultados" to "RESULTADOS" in calculadoras.blade.php

--- resources/views/calculadoras.blade.php
+++ resources/views/calculadoras.blade.php
@@ -48,7 +48,7 @@
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg my-4">
                 <div class="p-6 text-gray-900">
                     <h2 class="font-semibold text-xl text-gray-800 leading-tight flex  justify-center mb-2">
-                        {{ __('Resultados') }}
+                        {{ __('RESULTADOS') }}
                     </h2>
                     
                     <div id="resultsContainer" class="flex flex-wrap -mx-2 hidden">
@@ -116,6 +116,7 @@
                 @endif
             </div>
             @endif
+        </div>
 
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
 
@@ -123,7 +124,7 @@
                 <div class="px-16 py-6 text-gray-900">
                     <div class="flex justify-center">
                         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-                            {{ __('SAIBA MAIS') }}
+                            {{ __('ENTENDA SEUS RESULTADOS') }}
                         </h2>
                     </div>
                     
